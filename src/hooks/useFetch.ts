3cf55c91fa35c337c4a5b70590f97b524eccb6b5
Gabriel Araujo <gabrielaraujo.dev@gmail.com>
2020-09-05 13:24:39 -0300

    configure swr and integrate with server

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,17 @@
+import useSWR from 'swr';
+import api from '../services/api';
+
+interface IResponse {
+  data: any;
+  error: any;
+}
+
+export function useFetch<Data = any, Error = any>(url: string): IResponse {
+  const { data, error } = useSWR<Data, Error>(url, async url => {
+    const response = await api.get(url);
+
+    return response.data;
+  });
+
+  return { data, error };
+}
